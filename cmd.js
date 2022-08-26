7fe8148ac9c118472b55e646b290eee9de280a1d
meerhamzadev <hamzababar37@gmail.com>
2022-08-26 22:32:15 +0500

    📦 NEW: Add license writing feature

--- cmd.js
+++ cmd.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
 var mit = require('./mit.js')
+const fs = require('fs');
+const path = require('path')
 
-console.log(mit(process.argv[2], process.argv[3]))
+const license = mit(process.argv[2], process.argv[3]);
+fs.writeFileSync(path.join(process.cwd(), 'License'), license);
